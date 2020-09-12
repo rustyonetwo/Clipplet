28b9f326766d7f4d38e918543e8dbf5220883525
gkeithstanley <gkeithstanley@gmail.com>
2020-09-12 06:37:52 -0500

    Renamed form

--- Clipplet.cs
+++ Clipplet.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Clippet
 {
-    public partial class Form1 : Form
+    public partial class frmClippletMain : Form
     {
-        public Form1()
+        public frmClippletMain()
         {
             InitializeComponent();
         }
--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace Clippet
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmClippletMain());
         }
     }
 }
